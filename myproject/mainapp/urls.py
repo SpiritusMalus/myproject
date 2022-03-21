5daeac179ae30b6e62ac194e27693ccb0cec6c7d
Евгений <80955062+SpiritusMalus@users.noreply.github.com>
2022-03-22 00:04:29 +0300

    Registration andnew htmls (#1)
    Registr

--- myproject/mainapp/urls.py
+++ myproject/mainapp/urls.py
@@ -1,11 +1,15 @@
 from myproject import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 
 urlpatterns = [
     path('', main, name='main'),
+    path('register/', Registration.as_view(), name='register'),
+    path('', include('social_django.urls', namespace='social')),
+
+
 ]
 
 if settings.DEBUG:
--- myproject/mainapp/views.py
+++ myproject/mainapp/views.py
@@ -1,42 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .models import *
 from .forms import MyUserCreationForm
-from django.contrib import auth
 
 
 def main(request):
     context = {
         "context": Product.objects.all(),
     }
-    if request.method == 'POST':
-        form = MyUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            form = auth.authenticate(email=form.cleaned_data['email'], password=form.cleaned_data['password2'])
-            auth.login(request, form)
-            context['form'] = form
-            return render(request, "mainapp/products.html", context)
-    else:
-        form = MyUserCreationForm()
-        context['form'] = form
-        return render(request, "mainapp/products.html", context)
-
     return render(request, "mainapp/products.html", context)
 
 
-"""Test func"""
-# def register(request):
-# if request.method == 'POST':
-#     form = MyUserCreationForm(request.POST)
-#     if form.is_valid():
-#         form.save()
-#         form = auth.authenticate(email=form.cleaned_data['email'], password=form.cleaned_data['password2'])
-#         auth.login(request, form)
-#         return form
-# else:
-#     form = MyUserCreationForm()
-#     context = {
-#         'form': form,
-#     }
-#     return context
+class Registration(CreateView):
+    form_class = MyUserCreationForm
+    template_name = 'mainapp/register.html'
+    success_url = reverse_lazy('main')
+
+
+
